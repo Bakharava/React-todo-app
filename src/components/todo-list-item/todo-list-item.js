417d26c17312a8d183d64968ef275540332a1f80
Volha_Bystrova <olavlad777@gmail.com>
2020-11-20 11:26:49 +0300

    Create todo react app

---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './todo-list-item.css'
+
+export default class TodoListItem extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      done: false,
+      important: false,
+    };
+
+    /*this.onLabelClick = () => {
+      console.log(`Done ${this.props.label}`)
+    }*/
+  }
+
+  render() {
+    const { label,
+      onDeleted,
+      onToggleImportant,
+      onToggleDone,
+      important,
+      done } = this.props;
+
+    let classNames = 'todo-list-item';
+
+    if(done) {
+      classNames += ' done'
+    }
+
+   if (important) {
+     classNames +=' important'
+   }
+
+    return (
+      <span className={classNames}>
+      <span
+        className="todo-list-item-label"
+        onClick={ onToggleDone }>
+      { label }
+    </span>
+
+      <button type="button"
+              className="btn btn-outline-success btm-sm float-right"
+      onClick={onToggleImportant}
+      >
+        <i className="fa fa-exclamation"/>
+      </button>
+      <button type="button"
+              className="btn btn-outline-danger btm-sm float-right"
+              onClick={onDeleted}
+      >
+        <i className="fa fa-trash-o"/>
+      </button>
+    </span>
+
+    );
+  };
+
+}
